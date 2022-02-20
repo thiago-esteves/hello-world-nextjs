36db75f107b37136bd2f42cd19d1666e83c93294
thiago-esteves <81878335+thiago-esteves@users.noreply.github.com>
2022-02-20 01:32:53 -0300

    estudo de apis no next.js

---
+++ pages/api/data.ts
@@ -0,0 +1,8 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import moment from "moment"
+
+
+export default  (req : NextApiRequest, res : NextApiResponse) => {
+    res.status(200).json({data : new moment().format('DD/MM/yyyy HH.mm.ss')});
+}
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -3,11 +3,12 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string
+  idade: number
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: 'Thiago Esteves' , idade : 32});
 }
